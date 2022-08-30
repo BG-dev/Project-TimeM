3dd77626b7d7b21786ab1398996cbc02ddef26c8
BG-dev <nik.azizoff2016@gmail.com>
2022-08-30 18:56:39 +0300

    outside detect

--- Client/src/components/DropdownMenu/DropdownMenu.jsx
+++ Client/src/components/DropdownMenu/DropdownMenu.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import "./DropdownMenu.scss";
 
 function DropdownMenu({ options }) {
   const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+  }, []);
+
+  const refBlock = useRef(null);
+
+  const handleClickOutside = (e) => {
+    if (!refBlock.current.contains(e.target)) {
+      setActive(false);
+    }
+  };
 
   return (
     <div className="dropdown">
@@ -11,7 +25,7 @@ function DropdownMenu({ options }) {
         onClick={() => setActive((prev) => !prev)}
       ></i>
       {active && (
-        <div className="dropdown__menu">
+        <div ref={refBlock} className="dropdown__menu">
           <ul className="dropdown__list">
             {options &&
               options.map((option, index) => (
