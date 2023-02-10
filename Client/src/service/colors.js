f7d4d3018383a17e6595c766aaec655fb3bbe4ad
Nikita <57636373+BG-dev@users.noreply.github.com>
2023-02-10 20:19:45 +0300

    Update colors.js

--- Client/src/service/colors.js
+++ Client/src/service/colors.js
@@ -1,41 +1,4 @@
-// const colors = [
-//   {
-//     name: "blue",
-//     value: "#1e90ff",
-//   },
-//   {
-//     name: "orange",
-//     value: "#ffa500",
-//   },
-//   {
-//     name: "green",
-//     value: "#008000",
-//   },
-//   {
-//     name: "red",
-//     value: "#dc143c",
-//   },
-//   {
-//     name: "purple",
-//     value: "#9370db",
-//   },
-//   {
-//     name: "pink",
-//     value: "#ffc0cb",
-//   },
-//   {
-//     name: "lime",
-//     value: "#32cd32",
-//   },
-//   {
-//     name: "sky",
-//     value: "#87ceeb",
-//   },
-//   {
-//     name: "grey",
-//     value: "#a9a9a9",
-//   },
-// ];
+
 
 const colors = [
   "#1e90ff",
